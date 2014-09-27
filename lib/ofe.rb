1951bfa7aa36dac507fa29c81330550bbd21d626
Erik Nomitch <erik@nomitch.com>
2014-09-27 13:20:05 -0500

    parses config file

--- lib/ofe.rb
+++ lib/ofe.rb
@@ -1,6 +1,7 @@
 # ================================================
 # OFE ============================================
 # ================================================
+require "json"
 
 # ------------------------------------------------
 # MODULE->OFE ------------------------------------
@@ -8,8 +9,9 @@
 module Ofe
 
   # ----------------------------------------------
-  # ATTRIBUTES -----------------------------------
+  # MODULE->VARIABLES ----------------------------
   # ----------------------------------------------
+  @@config_json = nil
   
   # ----------------------------------------------
   # UTILITY --------------------------------------
@@ -35,22 +37,42 @@ module Ofe
     raise "fatal: No config file found at ./#{config_file_filename}"
   end
 
+  def self.parse_config_file
+    require_config_file
+
+    begin
+
+      @@config_json = JSON.parse(File.open(config_file_filename).read)
+    
+    rescue => exception
+      raise "Cannot parse 'ofe.json' because its JSON is invalid."
+    end
+
+  end
+
   # ----------------------------------------------
   # TARGET ---------------------------------------
   # ----------------------------------------------
-  def self.target
-    target = :default
-    target = ARGV.first.to_sym if ARGV.count > 0
-    target
+  def self.get_target
+    return :default if ARGV.count == 0
+
+    ARGV.first.to_sym
+  end
+
+  def self.valid_target?(target)
   end
   
   # ----------------------------------------------
   # FINDING --------------------------------------
   # ----------------------------------------------
+  def self.files_for_target(target)
+  end
   
   # ----------------------------------------------
   # OPENING --------------------------------------
   # ----------------------------------------------
+  def self.open_target(target=get_target)
+  end
 
   # ----------------------------------------------
   # MAIN -----------------------------------------
@@ -59,13 +81,16 @@ module Ofe
 
     begin
 
-      require_config_file
+      parse_config_file
+      open_target
 
-      puts target
+      puts @@config_json
 
     rescue => exception
+
       puts "#{current_file_basename}: #{exception}"
       exit 1
+
     end
   end
   
